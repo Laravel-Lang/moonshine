558823dc6ca17f81f8ee8f56e59d41c4781ae8a5
Andrey Helldar <helldar@dragon-code.pro>
2025-02-17 12:12:41 +0300

    Fixed translations

--- app/translate.php
+++ app/translate.php
@@ -14,13 +14,13 @@ function dotted(Arrayable $array): array
     return collect($array->toArray())->dot()->all();
 }
 
-function fromLang(string $locale, bool $isInline): array
+function fromLang(string $locale, bool $isInline, array $onlyKeys): array
 {
     $filename = $isInline ? 'php-inline.json' : 'php.json';
 
     $values = Arr::ofFile(__DIR__ . "/../vendor/laravel-lang/lang/locales/$locale/$filename");
 
-    return dotted($values);
+    return Arr::only(dotted($values), $onlyKeys);
 }
 
 function fromMoonShine(string $filename): array
@@ -34,21 +34,40 @@ function translated(string $locale, bool $isInline): array
 {
     $filename = $isInline ? 'php-inline.json' : 'php.json';
 
+    $path = __DIR__ . "/../locales/$locale/$filename";
+
+    if (! file_exists($path)) {
+        return [];
+    }
+
     $values = Arr::ofFile(__DIR__ . "/../locales/$locale/$filename");
 
     return dotted($values);
+}
+
+function merge(array ...$arrays): array
+{
+    $first = $arrays[0];
+
+    for ($i = 1; $i < count($arrays); $i++) {
+        foreach ($arrays[$i] as $key => $value) {
+            $first[$key] = $value;
+        }
+    }
+
+    return $first;
 }
 
 function storeTranslations(string $locale, array $values, bool $isInline): void
 {
     $filename = $isInline ? 'php-inline.json' : 'php.json';
 
-    file_put_contents(__DIR__ . "/../locales/$locale/$filename", json_encode($values, JSON_THROW_ON_ERROR));
+    $flags = JSON_THROW_ON_ERROR ^ JSON_PRETTY_PRINT ^ JSON_UNESCAPED_SLASHES ^ JSON_UNESCAPED_UNICODE;
+
+    file_put_contents(__DIR__ . "/../locales/$locale/$filename", json_encode($values, $flags));
 }
 
 foreach (Directory::names(__DIR__ . '/../locales') as $locale) {
-    $langTranslated       = fromLang($locale, false);
-    $langInlineTranslated = fromLang($locale, true);
 
     $moonshineAuth       = fromMoonShine('auth.php');
     $moonshineValidation = fromMoonShine('validation.php');
@@ -56,17 +75,24 @@ foreach (Directory::names(__DIR__ . '/../locales') as $locale) {
     $translated       = translated($locale, false);
     $translatedInline = translated($locale, true);
 
-    $keysAuth       = array_keys($moonshineAuth);
-    $keysValidation = array_keys($moonshineValidation);
+    $keys = array_keys(merge($moonshineAuth, $moonshineValidation));
 
-    $keys = Arr::merge($keysAuth, $keysValidation);
+    $lang       = fromLang($locale, false, $keys);
+    $langInline = fromLang($locale, true, $keys);
 
-    $merged = Arr::of($translated)->merge($langTranslated)->only($keys);
+    $merged       = merge($translated, $moonshineAuth, $moonshineValidation, $lang);
+    $mergedInline = merge($translatedInline, $moonshineAuth, $moonshineValidation, $langInline);
 
-    $mergedInline = Arr::of($translatedInline)->merge($langInlineTranslated)->only($keys)->filter(
-        fn (mixed $value) => Str::doesntContain($value, ':attribute', true)
+    $merged = array_filter(
+        $merged,
+        fn (mixed $value) => filled($value)
     );
 
-    storeTranslations($locale, $merged->toArray(), false);
-    storeTranslations($locale, $mergedInline->toArray(), true);
+    $mergedInline = array_filter(
+        $mergedInline,
+        fn (mixed $value) => filled($value) && Str::doesntContain($value, ':attribute', true)
+    );
+
+    storeTranslations($locale, $merged, false);
+    storeTranslations($locale, $mergedInline, true);
 }
